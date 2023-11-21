9c0cd73eabbd742aa6ab362f7a65a3c51a0c8d8a
Pashavilich <151665992+Pashavilich@users.noreply.github.com>
2023-11-21 20:03:43 +0200

    Update index.php

--- potfolio/index.php
+++ potfolio/index.php
@@ -47,10 +47,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" contnt="ie=edge">
         <title>Weather Scraper</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-              rel="stylesheet" 
-              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-              crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
         <!-- CSS -->
         <style type="text/css">
